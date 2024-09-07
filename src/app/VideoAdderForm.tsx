e5f82bdd8be6d99b9d1dad12ad669e3eab07528f
Den <daniel.gal@danielgal.eu>
2024-09-07 23:37:28 +0200

    protect from multiple submit

--- src/app/VideoAdderForm.tsx
+++ src/app/VideoAdderForm.tsx
@@ -5,13 +5,16 @@ import { trpc } from "~/trpc/client";
 
 export default function VideoAdderForm() {
   const [url, setUrl] = useState("");
-  const { mutate: addVideo } = trpc.addVideo.useMutation({
+  const { mutate: addVideo, isPending } = trpc.addVideo.useMutation({
     onSuccess: () => setUrl(""),
   });
   return (
     <form
       className="flex gap-4"
       onSubmit={(event) => {
+        if (isPending) {
+          return;
+        }
         event.preventDefault();
         addVideo({ url });
       }}
@@ -20,8 +23,11 @@ export default function VideoAdderForm() {
         type="text"
         value={url}
         onChange={(event) => setUrl(event.target.value)}
+        disabled={isPending}
       />
-      <button type="submit">Add Video</button>
+      <button type="submit" disabled={isPending}>
+        Add Video
+      </button>
     </form>
   );
 }

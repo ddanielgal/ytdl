ba0e2453ba8b3174f071535483cdfae606276f7f
Den <daniel.gal@danielgal.eu>
2024-10-23 14:33:43 +0200

    make available on subpath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/ytdl",
+  assetPrefix: "/ytdl/",
+  trailingSlash: false,
+};
 
 export default nextConfig;
